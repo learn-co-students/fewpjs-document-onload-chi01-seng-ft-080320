ca375d6731700ab0616b37d5e89840083b5ef5b5
AnansiOmega <ignasbutautas7@gmail.com>
2020-09-14 18:03:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    contentLoaded()
+})
+
+
+
+  function contentLoaded(){
+    let text = document.getElementById("text")
+    text.innerHTML = "This is really cool!"
+  }
