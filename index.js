73c29ce94a6f7fa6149fa2936337931354e9c5c5
thrlstl <matthewsteeleonline@gmail.com>
2020-09-17 22:43:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+function updateText(){
+    document.getElementById("text")
+    .innerHTML = "This is really cool!";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    updateText()
+  });
